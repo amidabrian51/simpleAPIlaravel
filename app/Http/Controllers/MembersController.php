76444a587873fa5c3e2e431d75d6ef546e800734
Brian Patterson <briankaty1@me.com>
2017-07-04 14:14:25 +0100

    Added error response code

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -17,9 +17,23 @@ class MembersController extends Controller
     //3. They are seeing the database structure. Not great.
     //4. What happens if we change the fields.
     //5. No way to signal headers/response codes
-    $members = Member::all();//not a great idea.
+    $member = Member::all();//not a great idea.
     return response()->json([
-      'data'=> $members->toArray(),
+      'data'=> $member->toArray(),
+    ], 200);
+  }
+
+  public function show($id)
+  {
+    $member = Member::find($id);
+
+    if (! $member) {
+      return response()->json([
+        'error' => ['Member does not exist']
+      ], 404);
+    }
+    return response()->json([
+      'data'=> $member->toArray(),
     ], 200);
   }
 }
